2257b78915846178cb4f7a1b8c94a5a1140c079d
Elmer Mauricio Chacon Osorio <treechacon@crecer.com.sv>
2019-08-08 15:05:20 -0600

    Setup routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+
+import { Routes} from '@angular/router';
+import { SectionSalesComponent } from './Sections/section-sales/section-sales.component';
+import { SectionOrdersComponent } from './Sections/section-orders/section-orders.component';
+import { SectionHealthComponent } from './Sections/section-health/section-health.component';
 
 
-const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'sales', component: SectionSalesComponent},
+    {path: 'orders', component: SectionOrdersComponent},
+    {path: 'health', component: SectionHealthComponent},    
+    {path: '', redirectTo:'/sales', pathMatch:'full'}
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from "@angular/router";
+import { routes } from "./app-routing.module";
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SectionSalesComponent } from './Sections/section-sales/section-sales.component';
+import { SectionOrdersComponent } from './Sections/section-orders/section-orders.component';
+import { SectionHealthComponent } from './Sections/section-health/section-health.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    SectionSalesComponent,
+    SectionOrdersComponent,
+    SectionHealthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
