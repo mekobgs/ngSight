04f757cdf1408544c0f0d9f012f305cc5f378cf6
Elmer Chacon <treechacon@crecer.com.sv>
2019-08-12 11:43:53 -0600

    Updated server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -12,9 +12,29 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color:string;
+  buttonText: string;
+
   @Input() server: Server;
   
   ngOnInit() {
+    this.setServerAction(this.server.isOnline);
+  }
+
+  setServerAction(isOnline: boolean){
+    if(isOnline){
+      this.server.isOnline = true;
+      this.color = '#66bb6a';
+      this.buttonText = 'Shut Down';
+    }else{
+      this.server.isOnline = false;
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean){
+    this.setServerAction(!onlineStatus);
   }
 
 }
